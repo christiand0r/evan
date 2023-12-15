f8606a9ddb9f7f16a0ac175a4c5326e2301369ce
Matias <m.paillalef.c@gmail.com>
2023-12-15 11:36:59 -0300

    refactor: config next

--- next.config.js
+++ next.config.js
@@ -7,21 +7,14 @@ const CMS_URL = process.env.CMS;
 const CURRENT_URL = process.env.HOST;
 
 const nextConfig = {
+    productionBrowserSourceMaps: true,
     images: {
-      domains: [CMS_URL, CURRENT_URL],
+        domains: [CMS_URL, CURRENT_URL],
     },
-    /*api: {
-        bodyParser: true, // Habilita el manejo de solicitudes POST en la carpeta 'api'
+    optimization: {
+        minimize: true, // Habilitar la minificación de código
     },
-    async rewrites() {
-        return [
-            // Ejemplo de reescritura para enviar todas las solicitudes '/api/sendEmail' a '/api/sendEmail.js'
-            {
-                source: '/api/:path*',
-                destination: '/api/:path*.js',
-            },
-        ];
-    },*/
+    reactStrictMode: false,
 };
 
 module.exports = nextConfig;
